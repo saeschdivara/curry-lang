6f042f64983add809df89a557d6f165bf7450199
Sascha Häusler <saeschdivara@gmail.com>
2022-11-19 22:07:30 +0100

    03 - Start with evaluation

---
+++ evaluator/evaluator.go
@@ -0,0 +1,31 @@
+package evaluator
+
+import (
+	"curryLang/ast"
+	"curryLang/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *ast.Program:
+		return EvalStatements(node.Statements)
+	}
+
+	return nil
+}
+
+func EvalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range statements {
+		result = Eval(stmt)
+	}
+
+	return result
+}
