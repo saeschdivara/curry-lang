8d4b3007b47033428e52d9d08246ec0d43a699b4
Sascha Häusler <saeschdivara@gmail.com>
2022-11-26 15:24:13 +0100

    Compiler - Add better debugging code

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -4,6 +4,7 @@ import (
 	"curryLang/ast"
 	"curryLang/code"
 	"curryLang/lexer"
+	"curryLang/object"
 	"curryLang/parser"
 	"fmt"
 	"testing"
@@ -70,6 +71,40 @@ func testInstructions(
 			return fmt.Errorf("wrong instruction at %d.\nwant=%q\ngot =%q", i, concatted, actual)
 		}
 	}
+	return nil
+}
+
+func testConstants(t *testing.T,
+	expected []interface{},
+	actual []object.Object,
+) error {
+	if len(expected) != len(actual) {
+		return fmt.Errorf("wrong number of constants. got=%d, want=%d", len(actual), len(expected))
+	}
+
+	for i, constant := range expected {
+		switch constant := constant.(type) {
+		case int:
+			err := testIntegerObject(int64(constant), actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+			}
+		}
+	}
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+	}
+
 	return nil
 }
 
