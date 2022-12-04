7c348b4cde6c221e121f822920595e344833ec46
Sascha Häusler <saeschdivara@gmail.com>
2022-12-04 21:43:07 +0100

    Remove redundant code

--- compiler/symbol_table_test.go
+++ compiler/symbol_table_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestDefine(t *testing.T) {
 	expected := map[string]Symbol{
-		"a": Symbol{Name: "a", Scope: GlobalScope, Index: 0},
-		"b": Symbol{Name: "b", Scope: GlobalScope, Index: 1},
+		"a": {Name: "a", Scope: GlobalScope, Index: 0},
+		"b": {Name: "b", Scope: GlobalScope, Index: 1},
 	}
 	global := NewSymbolTable()
 	a := global.Define("a")
@@ -17,13 +17,14 @@ func TestDefine(t *testing.T) {
 		t.Errorf("expected b=%+v, got=%+v", expected["b"], b)
 	}
 }
+
 func TestResolveGlobal(t *testing.T) {
 	global := NewSymbolTable()
 	global.Define("a")
 	global.Define("b")
 	expected := []Symbol{
-		Symbol{Name: "a", Scope: GlobalScope, Index: 0},
-		Symbol{Name: "b", Scope: GlobalScope, Index: 1},
+		{Name: "a", Scope: GlobalScope, Index: 0},
+		{Name: "b", Scope: GlobalScope, Index: 1},
 	}
 	for _, sym := range expected {
 		result, ok := global.Resolve(sym.Name)
